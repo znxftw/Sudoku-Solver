b1d757b92e1fcb811f1b55132f5d349d02289303
Vishnu Bhagyanath <vishnu2101@gmail.com>
2017-02-23 13:19:31 +0530

    Added comments
    Added comments regarding functions to test out commits on GitHub

--- Sudoku.py
+++ Sudoku.py
@@ -1,11 +1,12 @@
 from math import ceil
+#Parse True/False values of initial Grid
 def parseInit():
     for i in range(9):
         for j in range(9):
             if Grid[i][j]!=0 :
                 boolGrid[i][j]=[False for k in range(9)]
                 boolGrid[i][j][Grid[i][j]-1]=True
-
+#Sum up the truth values for all elements
 def checkCompletion():
     return int(sum(sum(sum(j) for j in i) for i in boolGrid))
 
@@ -14,6 +15,7 @@ def filterPresent():
         for j in range(9):
             if Grid[i][j]!=0:
                     unTrueRest(i,j,Grid[i][j])
+#Same column,row,3x3 box gets current value position set to False
 def unTrueRest(i,j,n):
     for k in range(9):
 		if k!=j:
@@ -23,6 +25,7 @@ def unTrueRest(i,j,n):
 		for l in range(9):		
 			if(ceil((k+1)/3.0)==ceil((i+1)/3.0) and i!=k and ceil((l+1)/3.0)==ceil((j+1)/3.0) and l!=j):
 				boolGrid[k][l][n-1]=False
+#If we are certain of a number, fill it in Grid
 def eval():
 	for i in range(9):
 		for j in range(9):
@@ -30,6 +33,7 @@ def eval():
 				for k in range(9):
 					if boolGrid[i][j][k]==True:
 						Grid[i][j]=k+1
+#Print the 9x9 Grid
 def printSudoku():
 	for i in range(9):
 		for j in range(9):
